c81e5b290ce7775cd45926f38121da61e6f1585c
Ridwan Olawumi <olarid7852@gmail.com>
2024-12-05 12:58:36 +0100

    Removed pgclient

--- pg_client/create_db.sh
+++
@@ -1,2 +0,0 @@
-SELECT 'CREATE DATABASE obstracts' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'obstracts')\gexec
-SELECT 'CREATE DATABASE history4feed' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'history4feed')\gexec
